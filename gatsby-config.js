ddb6e1a3fb182389f6c7cfbf88498a2a7ac366cc
ponylight <mhuangzg@me.com>
2019-12-23 23:54:53 +1100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'EARTH',
+    description: 'ADA Inground Staking Pool',
+    siteUrl: 'https://www.adainground.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
