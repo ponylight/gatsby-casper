f988c6e8d22d447e7a0abd2bf54d50e0e63be7cd
ponylight <mhuangzg@me.com>
2019-12-23 23:56:28 +1100

    Update website-config.ts

--- src/website-config.ts
+++ src/website-config.ts
@@ -49,15 +49,15 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'EARTH',
+  description: 'ADA Inground Staking Pool',
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
+  siteUrl: 'https://www.adainground.com',
   facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
-  showSubscribe: true,
+  twitter: 'https://twitter.com/adainground',
+  showSubscribe: false,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
