0c00d4aa7c2f0cdfd259dd4ec3f7d3ae7e207265
ponylight <mhuangzg@me.com>
2019-12-26 00:58:50 +1100

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -37,39 +37,25 @@ const About: React.FC = () => (
           <PostFullContent className="post-full-content">
             <div className="post-content">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo finibus leo,
-                non tempus magna vehicula ac. Maecenas mollis ante finibus pharetra imperdiet.
-                Maecenas in aliquam purus. Nam et massa a nulla fermentum dapibus sit amet in neque.
-                Ut ipsum ipsum, rhoncus a sodales pellentesque, interdum a elit. Nullam aliquam
-                tellus nibh, eget laoreet dui aliquet non. Vestibulum malesuada ante at diam tempus,
-                ac interdum risus scelerisque. Sed ipsum neque, vulputate porta diam eget, consequat
-                blandit nulla. Integer volutpat velit vitae purus lacinia aliquam. Integer bibendum
-                ipsum vitae magna pulvinar, nec vehicula dolor vulputate. Nulla eu massa id orci
-                placerat finibus vel sit amet eros. Vestibulum quis consequat massa. Sed sagittis
-                sollicitudin massa at commodo. Praesent diam nisi, imperdiet posuere eleifend nec,
-                blandit ac massa.
+                We are professional network and system engineers with extensive experience, and
+                we have been working on Cardano testnet from beginning.
               </p>
               <p>
-                Vestibulum semper pretium ipsum nec congue. Ut ac eros nisi. Donec leo sem, aliquam
-                mollis sapien ultrices, dapibus congue diam. Proin viverra dapibus blandit. Ut
-                mauris tellus, tristique id felis vel, venenatis vestibulum nunc. Nam molestie
-                pulvinar nibh, eget egestas augue. Maecenas tellus arcu, mattis ut ipsum non,
-                sollicitudin convallis nunc. Donec nec neque tristique, aliquet lacus id, laoreet
-                nunc. Cras dapibus nisi nulla, ullamcorper faucibus neque suscipit ac. Donec eget
-                orci venenatis justo lobortis volutpat. Proin vel placerat nisl. Integer arcu nunc,
-                sodales eu fringilla non, aliquam non diam. Cras placerat, massa et faucibus
-                pretium, ante elit tincidunt tellus, tristique ultricies velit quam et massa.
+                To diversify the infrastructure for Cardano Ecosystem and brace decentralization, and
+                be prepared for the potential exploding requirements, we built the EARTH Inground Stake
+                Pool on a Cisco UCS C-Series Server with dual Intel Xeon CPUs and 64GB Memory hosted
+                in our data center. The server is connected to a 1 Gbps dedicated Internet link through
+                a Cisco Security Appliance.
               </p>
               <p>
-                In nunc lacus, dapibus vitae lacus sit amet, efficitur iaculis neque. Suspendisse ut
-                tellus quis leo vestibulum tincidunt. Aenean nec enim ac dolor lacinia semper. Ut
-                sed laoreet libero. Nunc elementum sollicitudin accumsan. Nunc eu augue neque. Proin
-                a tortor nibh. Cras eu nisl ornare sapien feugiat pellentesque. Mauris dignissim vel
-                quam eu pellentesque. Integer sit amet posuere quam, eu ullamcorper odio. Nullam a
-                lacus tempus sapien dignissim ullamcorper. In hac habitasse platea dictumst. Proin
-                quis massa aliquam, feugiat tortor sit amet, tincidunt urna. Donec posuere pulvinar
-                lectus, ac semper ipsum vulputate quis.
+                With the income from staking, we are planning to reinvest to build the state of art ADA
+                staking pool with DDoS Protection, Next Gen Firewall, Malware Protection and Hyper
+                Convergence Infrastructure hosting in dual data centers for high availability.
               </p>
+              <p>
+                So please support us and stake with us. Our ticker name is <b>EARTH</b>, and our
+                pool ID is "<b>715ff93c6eda45773d05862eb6fa57a798125af18fe6c0f66d0dbb26cee876eb</b>".
+              </P>
             </div>
           </PostFullContent>
         </article>
