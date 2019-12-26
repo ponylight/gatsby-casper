7ab8632df366e1dd882802d589e651a74bbef1f1
ponylight <mhuangzg@me.com>
2019-12-26 20:53:02 +1100

    Update website-config.ts

--- src/website-config.ts
+++ src/website-config.ts
@@ -55,7 +55,7 @@ const config: WebsiteConfig = {
   logo: 'img/ghost-logo.png',
   lang: 'en',
   siteUrl: 'https://www.adainground.com',
-  facebook: 'https://www.facebook.com/ghost',
+  facebook: 'https://www.facebook.com/adainground',
   twitter: 'https://twitter.com/adainground',
   showSubscribe: false,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
